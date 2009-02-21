622a9dbd06d0f29a2952d765af5b4473dafa4369
Jason Marcell <jason@jason-marcells-macbook.local>
2009-02-20 23:58:03 -0500

    work

--- main.c
+++ main.c
@@ -10,17 +10,74 @@ int sjf_comparison(void *a, void *b)
     return (((job*)b)->burst - ((job*)a)->burst);
 }
 
-int main (int argc, char const *argv[])
+int main (int argc, char *argv[])
 {
-    heap *h = malloc(sizeof(heap));
-    heap *c = malloc(sizeof(heap));
-    heap_init(h);
-    heap_init(c);
-//    generate_jobs(h, fcfs_comparison, 5);
-    read_jobs_from_file(h, fcfs_comparison, "data.txt");
-    printf("number of jobs: %d\n", h->size);
-    print_jobs(h);
-    process_jobs(h, fcfs_comparison, c);
-    print_results(c);
+    char *subopts, *value;
+    int opt,
+        sjf = 0,
+        fcfs = 0;
+    FILE *fp = NULL;
+    
+    if(argc == 1) {
+        print_usage(argc, argv);
+        return 1;
+    }
+    while ((opt = getopt(argc, argv, "i:s:")) != -1)
+        switch (opt)
+    {
+        case 'i': 
+            fp = fopen(optarg, "r");
+            break;
+        case 's':
+            subopts = optarg;
+            while (*subopts != '\0')
+                switch (getsubopt (&subopts, scheduler_opts, &value))
+            {
+                case SJF:
+                    sjf = 1;
+                    break;
+                case FCFS:
+                    fcfs = 1;
+                    break;
+                default:
+                    printf ("Unknown suboption `%s'\n", value);
+                    break;
+            }
+            break;
+        default:
+            print_usage(argc, argv);
+            abort();
+            break;
+    }
+
+    if(sjf) {
+        heap *h = malloc(sizeof(heap));
+        heap *c = malloc(sizeof(heap));
+        heap_init(h);
+        heap_init(c);
+        if(fp)
+            read_jobs_from_file(h, sjf_comparison, fp);
+        else
+            generate_jobs(h, sjf_comparison, 5);
+        printf("number of jobs: %d\n", h->size);
+        print_jobs(h);
+        process_jobs(h, sjf_comparison, c);
+        print_results(c);
+    }
+    
+    if(fcfs) {
+        heap *h = malloc(sizeof(heap));
+        heap *c = malloc(sizeof(heap));
+        heap_init(h);
+        heap_init(c);
+        if(fp)
+            read_jobs_from_file(h, fcfs_comparison, fp);
+        else
+            generate_jobs(h, fcfs_comparison, 5);
+        printf("number of jobs: %d\n", h->size);
+        print_jobs(h);
+        process_jobs(h, fcfs_comparison, c);
+        print_results(c);
+    }
     return 0;
 }
--- main.h
+++ main.h
@@ -1,12 +1,13 @@
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include <inttypes.h>
-#include <sys/types.h>
 #include <limits.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 #include "heap.h"
 
 typedef struct {
@@ -19,14 +20,27 @@ typedef struct {
         priority;
 } job;
 
+enum {
+  SJF = 0,
+  FCFS,
+  THE_END
+};
+
+const char *scheduler_opts[] = {
+  [SJF] = "sjf",
+  [FCFS] = "fcfs",
+  [THE_END] = NULL
+};
+
 void build_job(job *j, int id, int arrive, int burst, int priority);
 void increment_waits(heap *h);
 void generate_jobs(heap *h, int (*comp_func)(void*, void*), int number_of_jobs);
-void read_jobs_from_file(heap *h, int (*comp_func)(void*, void*), char* filename);
+void read_jobs_from_file(heap *h, int (*comp_func)(void*, void*), FILE *fp);
 void process_jobs(heap *h, int (*comp_func)(void*, void*), heap *c);
 void print_jobs(heap *h);
 void print_job(job *j);
 void print_results(heap *c);
+void print_usage(int argc, char *argv[]);
 
 void build_job(job *j, int id, int arrive, int burst, int priority)
 {
@@ -65,7 +79,7 @@ void generate_jobs(heap *h, int (*comp_func)(void*, void*), int number_of_jobs)
     return;
 }
 
-void read_jobs_from_file(heap *h, int (*comp_func)(void*, void*), char* filename)
+void read_jobs_from_file(heap *h, int (*comp_func)(void*, void*), FILE *fp)
 {
     char buffer[256];
     int i = 0,
@@ -73,7 +87,6 @@ void read_jobs_from_file(heap *h, int (*comp_func)(void*, void*), char* filename
         burst,
         priority;
 
-    FILE *fp = fopen(filename, "r");
     while (!feof(fp)) {
         fgets(buffer, 256, fp);
         arrive = strtol(strtok(buffer, ",\n"), NULL, 10);
@@ -135,5 +148,14 @@ void print_results(heap *c)
     for(j = 1; j <= c->size; j++)
         sum_waiting += ((job*)(c->a[j]))->waiting;
     printf("average waiting time: %3.2f\n", (float)sum_waiting / (float)c->size);
+    return;
+}
+
+void print_usage(int argc, char *argv[])
+{
+    printf("usage: %s [OPTIONS]\n", argv[0]);
+    printf("\t-i <file>\t\tRead comma-separated file with arrive,burst,priority\n");
+    printf("\t-s <scheduler(s)>\tSpecify scheduler(s) to use.\n");
+    printf("\t\t\t\tValid schedulers are: sjf, fcfs\n");
     return;
 }
